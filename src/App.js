6a334a852baa6a078df93872f269b5463dd74e28
Ndene SENE <ndenenafi10@gmail.com>
2020-08-27 15:56:58 +0000

    GraphQl checklist projet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,144 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_TODOS = gql`
+  query getTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
+
+const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: uuid!, $done: Boolean!) {
+    update_todos(where: { id: { _eq: $id } }, _set: { done: $done }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+const ADD_TODO = gql`
+  mutation addTodo($text: String!) {
+    insert_todos(objects: { text: $text }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: uuid!) {
+    delete_todos(where: { id: { _eq: $id } }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`;
+
+// List todos
+// Add todo
+// Toggle todo
+// Delete todo
 
 function App() {
+  const [todoText, setTodoText] = React.useState("");
+  const { data, loading, error } = useQuery(GET_TODOS);
+  const [toggleTodo] = useMutation(TOGGLE_TODO);
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () => setTodoText(""),
+  });
+  const [deleteTodo] = useMutation(DELETE_TODO);
+
+  async function handleToggletodo({ id, done }) {
+    const data = await toggleTodo({
+      variables: { id, done: !done },
+    });
+    console.log("Toggle todo", data);
+  }
+
+  async function handleAddTodo(event) {
+    event.preventDefault();
+    if (!todoText.trim()) return;
+    const data = await addTodo({
+      variables: { text: todoText },
+      refetchQueries: [
+        {
+          query: GET_TODOS,
+        },
+      ],
+    });
+    console.log("Ajout todo", data);
+  }
+
+  async function handleDeleteTodo({ id }) {
+    const isConfirmed = window.confirm("Vous voulez vraiment supprimer ?");
+    if (isConfirmed) {
+      const data = await deleteTodo({
+        variables: { id },
+        update: (cache) => {
+          const prevData = cache.readQuery({
+            query: GET_TODOS,
+          });
+          const newTodos = prevData.todos.filter((todo) => todo.id !== id);
+          cache.writeQuery({ query: GET_TODOS, data: { todos: newTodos } });
+        },
+      });
+      console.log("Delete todo", data);
+    }
+  }
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error fetching todos...</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">
+        GraphQL CheckList{" "}
+        <span role="img" aria-label="Checkmark">
+          ✅
+        </span>{" "}
+      </h1>
+      <form className="mb3" onSubmit={handleAddTodo}>
+        <input
+          className="pa2 f4 b--dashed"
+          type="text"
+          placeholder="Write your todo"
+          onChange={(event) => setTodoText(event.target.value)}
+          value={todoText}
+        />
+        <button className="pa2 f4 bg-green" type="submit">
+          Create
+        </button>
+      </form>
+      {/* {Todo list} */}
+      <div className="flex items-center justify-center flex-column">
+        {data.todos.map((todo) => (
+          <p onDoubleClick={() => handleToggletodo(todo)} key={todo.id}>
+            <span className={`pointer list pa1 f3 ${todo.done && "strike"}`}>
+              {todo.text}
+            </span>
+            <button
+              className="bg-transparent bn f4"
+              onClick={() => handleDeleteTodo(todo)}
+            >
+              {" "}
+              <span className="red">&times;</span>{" "}
+            </button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "https://literate-orca-93.hasura.app/v1/graphql",
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </ApolloProvider>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById("root")
+);
